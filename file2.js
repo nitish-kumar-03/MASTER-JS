19a4bb5e17a3d488aa8251a02d827e66a895d992
Nitish Kumar <nk9990207902@gmail.com>
2024-12-03 10:47:14 +0530

    video 5 complete

---
+++ file2.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello", 3+3); // we are using node js not browser
+
+console.log(3+3); console.log("Nitish") // bad readablity
+
+let name = "Nitish"; // String
+let age = 21; // Number
+let isLoggedIn = false; // Boolean
+let value = null; // standalone
+let definition = undefined; // not defined
+// symbol // uniqueness
+
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
