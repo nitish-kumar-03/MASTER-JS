08114191cde0f3b3e982f7e7f84ced0369c0d298
Nitish Kumar <nk9990207902@gmail.com>
2024-12-04 15:43:36 +0530

    video 6 complete

---
+++ file3.js
@@ -0,0 +1,24 @@
+let score = "33abc";
+console.log(typeof(score))
+
+let score_num = Number(score) // string to number conversion
+console.log(score_num) // NaN not a number
+console.log(typeof(score_num))
+
+
+// "33" = 33
+// "33ab" = NaN
+// true = 1; false =0
+// null =0
+
+let islogg =1
+let islogg_bool = Boolean(islogg);
+console.log(islogg_bool)
+
+// "" = false , "nitish" = true
+
+
+let num = 33
+let string_sum = String(num)
+console.log(string_sum)
+console.log(typeof(string_sum))
