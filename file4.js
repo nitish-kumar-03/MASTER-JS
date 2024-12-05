8b0b3197f68c73f178de4d8cac7b6b825c0a8887
Nitish Kumar <nk9990207902@gmail.com>
2024-12-05 14:25:26 +0530

    video 7 complete

---
+++ file4.js
@@ -0,0 +1,7 @@
+let val =3 ;
+let negval =  -val;
+console.log(negval);
+console.log(val**val); // 3^3
+console.log("1"+2 +2) // 122
+console.log(1+2 +"2") // 32 string to number conversion is confusing
+console.log(+true); //1
