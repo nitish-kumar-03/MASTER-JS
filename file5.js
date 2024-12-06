d95002ba22a43285cdf4a697ac21cafe2195b88b
Nitish Kumar <nk9990207902@gmail.com>
2024-12-06 10:54:56 +0530

    video 8 complete

---
+++ file5.js
@@ -0,0 +1,7 @@
+console.log("9"==8); // make sure while comparing data type should be equal
+console.log(null==0);
+console.log(null<=0);
+console.log(null>=0);
+
+// === check value and data type both
+console.log("9"===9);
